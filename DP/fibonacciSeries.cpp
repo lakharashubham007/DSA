6d93fdc61ec0983ce751620591f8b008fd3f3891
shubham <lakharashubham623@gmail.com>
2024-04-21 09:35:19 +0530

    bottom up approach for fibonacci series

--- DP/fibonacciSeries.cpp
+++ DP/fibonacciSeries.cpp
@@ -1,3 +1,4 @@
+//Topdown approach
 class Solution {
 public:
  int topDownSolve(int n, vector<int>& dp){
@@ -17,5 +18,29 @@ public:
         vector<int> dp(n+1,-1);
         int ans = topDownSolve(n, dp);
         return ans;
+    }
+};
+
+
+//Bottom Up approach
+class Solution {
+public:
+  int bottimUpSolve( int n){
+    vector<int> dp(n+1,-1);
+    dp[0] = 0;
+    if(n==0)
+    return dp[0];
+    dp[1] = 1;
+
+
+    for(int i=2; i<=n; i++){
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+
+    return dp[n];
+  }
+
+    int fib(int n) {
+        return bottimUpSolve(n);
     }
 };
